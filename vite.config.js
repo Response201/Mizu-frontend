1abb908b74b97197f75cfeeca83f179b35e1f5fc
jess <jessica.response201@gmail.com>
2024-12-16 22:43:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      external: ['@fortawesome/free-solid-svg-icons', '@fortawesome/free-regular-svg-icons'],
-    },  // This is the default, but make sure it's set
+      // Ta bort `external` för FontAwesome-ikoner eftersom de inte ska externaliseras
+      external: [], // Om du inte har specifika externa beroenden att lägga till, lämna tomt
+    },
   },
-  base: '/',  // This ensures routes are handled properly
+  base: '/',  // Den här inställningen ser till att dina rutter hanteras korrekt
+  optimizeDeps: {
+    // För att se till att de specifika FontAwesome-modulerna optimeras under utveckling
+    include: ['@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons', '@fortawesome/free-regular-svg-icons'],
+  },
 });
